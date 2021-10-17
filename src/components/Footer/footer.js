54bd7dccf571038b3f4c9599e307987714645edf
Justin <justin900429@gmail.com>
2021-10-17 22:51:52 +0800

    Files

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,9 @@ const Footer = () => {
     return (
         <footer>
             <p>
-                © 2021 Copyright: NYCU ECE | Developed by
+                © 2021 Copyright: NYCU ECE | Developed by&nbsp;
+                <a href="https://github.com/Justin900429"
+                   target="_blank" rel="noreferrer">Justin</a> and&nbsp;
                 <a href="https://github.com/JoyceFang1213"
                    target="_blank" rel="noreferrer">Joyce</a>
             </p>
