78b9d10f8c65b0225ec224905bb63f4551620ff1
Justin <justin900429@gmail.com>
2021-10-18 13:49:00 +0800

    Files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import NavbarCustom from "./components/Navbar/navbar";
 import {useState} from "react";
-import "./App.css"
+import classes from "./App.module.css"
 import List from "./components/List/List";
 import Footer from "./components/Footer/footer";
 import Note from "./components/Note/note"
@@ -18,7 +18,7 @@ function App(props) {
     const setGradeClick = (value) => {
         if (value === "readme") {
             setShowList(false);
-        } else{
+        } else {
             setShowList(true);
             const decodeGrade = (value) => {
                 if (value === "大一") return "first";
@@ -34,15 +34,38 @@ function App(props) {
 
     const setTextChange = (text) => {
         setShowList(true);
+        if (text === "") {
+            setDisplayData(props.ori);
+            return;
+        }
+
         const reg = new RegExp(text, "gi");
 
-        const filter_list = props.ori.filter(data => {
-            for (let i = 0; i < data.length; ++i) {
-                if (data[i].match(reg)) return true;
+        const filter_hl_list = props.ori.reduce((filter, data) => {
+            let match = false;
+            for (let i = 0; i < data.length - 1 && !match; ++i) {
+                if (data[i].match(reg)) {
+                    match = true;
+                }
             }
-            return false;
-        })
-        setDisplayData(filter_list);
+
+            if (match) {
+                filter.push(data.map((col, idx) => {
+                    if (idx === 6) return col;
+                    const result = reg.exec(col);
+                    if (result) {
+                        return <span>{col.substr(0, result.index)}<span
+                            className={classes["hl"]}>{col.substr(result.index, result[0].length)}</span>{col.substr(result.index + result[0].length)}</span>
+                    } else {
+                        return col;
+                    }
+                }))
+            }
+
+            return filter;
+        }, [])
+
+        setDisplayData(filter_hl_list);
     }
 
     return (
@@ -51,9 +74,9 @@ function App(props) {
                 stickHandler={setStickHandler} gradeClick={setGradeClick}
                 textChange={setTextChange}
             />
-            { showList ?
-                <List display={showList? "block" : "none" } stickyTop={height} items={displayData}/> :
-                <Note display={showList? "none" : "block" }/>
+            {showList ?
+                <List display={showList ? "block" : "none"} stickyTop={height} items={displayData}/> :
+                <Note display={showList ? "none" : "block"}/>
             }
             <Footer/>
         </div>
